8d40994165b1ec07e0e5103c296c0b0a2cb4218d
Stefan Mitrović <stefmit10@gmail.com>
2021-02-23 13:10:53 +0100

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var usersToReturn = await _userRepository.GetMembersAsync();
+
+            return Ok(usersToReturn);
+        }
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUser(string username)
+        {
+            // var user = await _userRepository.GetMemberAsync(username);
+            // return user;
+            return await _userRepository.GetUserByUsernameAsync(username);
+        }
+    }
+}
